850a633879f7416f985246ffea089141ee1273d5
Victor Efrain Alarcon Olmedo <alarconolmedo99@gmail.com>
2025-08-12 02:13:05 -0500

    corrigiendo el fetch de registro en front

--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -41,7 +41,7 @@ export default function Registro({ onRegistro }) {
  const enviarFormulario = async () => {
   setEnviando(true);
   try {
-   const respuesta = await fetch("https://server-k8rp.onrender.com/usuario", {
+   const respuesta = await fetch("https://backend-15sl.onrender.com/usuario", {
     method: "PUT",
     headers: {
      "Content-Type": "application/json",
